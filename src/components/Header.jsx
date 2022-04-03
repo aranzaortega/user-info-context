b1793a2d432a2dfba0ef265759072427002679cf
aranzaortega <aranzaiortegas@gmail.com>
2022-04-03 22:49:07 +0200

    Nav bar center

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="mx-auto">
-        <h2 className="navbar-brand">User Welcome - Context App</h2>
+    <nav class="navbar navbar-dark bg-dark box-shadow">
+      <div className="container justify-content-center">
+        <a class="navbar-brand">User Welcome - Context App</a>
+      </div>
+      <div className="container justify-content-center">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              About
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
